315af4f3daac8cfd143bb93e5a32fe5c2115fb5d
Nabil Islam Hamja <hamjanabil11@gmail.com>
2025-06-18 00:28:19 +0600

    add flowbite

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,34 @@ function App() {
 
 
   return (
-    <div className='flex '>
-      <div className="w-[55%] pl-[190px] pt-[50px]">
+    <div className='flex flex-wrap '>
+      <div className=" lg:w-[55%] pl-[190px] pt-[50px]">
         <h2 className='font-bold font-nunito text-[34px] text-primary  pb-[13px]  '>Get started with easily register</h2>
         <p className='font-reguler  font-nunito text-[20px] text-black/50 '>Free register and you can enjoy it</p>
 
         <div className="mt-[60px]">
-          <div className=" relative">
-            <input className='border 2 border-primary/30 py-[26px] px-[45px] w-[368px] rounded-[9px] tracking-[4px] ' type="text" placeholder='Enter your email address' />
-            <label className='absolute top-[-13px] left-[34px]  bg-white px-[12px]  font-semibold font-nunito text-[13px text-primary ' >Email Address
-            </label>
+          <div class="relative my-[34px]">
+            <input type="text" id="floating_outlined2" className="block px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer" placeholder=" " />
+            <label for="floating_outlined2" class="absolute text-sm text-secondary/70 duration-300 transform  -translate-y-4 top-2 z-10 origin-[0] bg-white  px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4">Email Addresh</label>
           </div>
 
 
-          <div className=" relative my-[56px]" >
-            <input className='border 2 border-primary/30 py-[26px] px-[45px] w-[368px] rounded-[9px] tracking-[4px] ' type="text" placeholder='Enter your full name' />
-            <label className='absolute top-[-13px] left-[34px]  bg-white px-[12px] font-semibold font-nunito text-[13px text-primary ' >Ful name
-            </label>
+          <div class="relative my-[34px]">
+            <input type="text" id="floating_outlined2" className="block px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer" placeholder=" " />
+            <label for="floating_outlined2" class="absolute text-sm text-secondary/70 duration-300 transform  -translate-y-4 top-2 z-10 origin-[0] bg-white  px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4">Full Name</label>
           </div>
-          <div className=" relative">
-            <input className='border 2 border-primary/30 py-[26px] px-[45px] w-[368px] rounded-[9px] tracking-[4px] ' type="text" placeholder='Enter your password' />
-            <label className='absolute top-[-13px] left-[34px]  bg-white px-[12px]  font-semibold font-nunito text-[13px text-primary  ' >Password
-            </label>
+             <div class="relative my-[34px]">
+            <input type="text" id="floating_outlined2" className="block px-[26px] py-[26px] xl:w-[368px] text-xl text-secondary font-semibold bg-transparent rounded-lg border-2 border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-secondary/30 peer" placeholder=" " />
+            <label for="floating_outlined2" class="absolute text-sm text-secondary/70 duration-300 transform  -translate-y-4 top-2 z-10 origin-[0] bg-white  px-4 peer-focus:px-4 peer-focus:text-secondary/70  peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-2/5 rtl:peer-focus:left-auto start-4">Password</label>
           </div>
           <div className="w-[368px] text-center ">
-                  <button className=' w-full font-semibold font-nunito text-[20px text-white py-[20px]  px-[150px] bg-black rounded-[86px] mt-[51px]  ' >Sign up</button>
-                  <p className='font-reguler font-primary text-[13px] text-secondary  pt-[35px]'>Already  have an account ?<span className='text-orange-500 font-bold cursor-pointer'>Sign In</span> </p>
-                 
+            <button className=' w-full font-semibold font-nunito text-[20px text-white py-[20px]  px-[150px] bg-black rounded-[86px] mt-[51px]  ' >Sign up</button>
+            <p className='font-reguler font-primary text-[13px] text-secondary  pt-[35px]'>Already  have an account ?<span className='text-orange-500 font-bold cursor-pointer'>Sign In</span> </p>
+
           </div>
         </div>
       </div>
-      <div className="w-[45%] ">
+      <div className=" lg:w-[45%] ">
         <img className='w-full h-screen object-cover ' src={registration} alt="#registration" />
       </div>
     </div>
